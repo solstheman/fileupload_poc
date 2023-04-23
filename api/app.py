a0246b2ae14b0edfc294b5dd6253fa1c0d5fb379
Solomon Park <solomon@rentable.com>
2023-04-23 19:25:23 -0400

    Saving to db

--- api/app.py
+++ api/app.py
@@ -1,11 +1,23 @@
 from flask import Flask, jsonify, request
 import pandas as pd
+from flask_mysqldb import MySQL
+from flask_migrate import Migrate
 app = Flask(__name__)
 
+mysql = MySQL(app)
 
-@app.route('/')
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'password'
+app.config['MYSQL_DB'] = 'fileuploader_poc'
+
+migrate = Migrate(app, mysql)
+
+
+@app.route('/migrate')
 def index():
-    return 'Working'
+    create_tables()
+    return 'success'
 
 
 @app.route('/save', methods=['POST'])
@@ -14,4 +26,18 @@ def save_data():
     customer_data = pd.read_csv(file)
     print(customer_data.head())
 
+    cursor = mysql.connection.cursor()
+    cursor.execute(''' INSERT INTO user_data (name, email) VALUES(%s,%s)''',
+                   ('test', 'test@example.com'))
+    mysql.connection.commit()
+    cursor.close()
+
     return jsonify({'result': 'success'})
+
+
+def create_tables():
+    cursor = mysql.connection.cursor()
+    cursor.execute(
+        ''' CREATE TABLE user_data (id INT NOT NULL PRIMARY KEY AUTO_INCREMENT, name VARCHAR(40), email VARCHAR(40))''')
+    mysql.connection.commit()
+    cursor.close()
