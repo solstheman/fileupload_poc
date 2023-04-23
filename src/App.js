3f5531c4abb08764b19ca4352f020f8c8bbdf6f0
Solomon Park <solomon.m.park@gmail.com>
2023-04-23 14:18:57 -0400

    Working on permissions

--- src/App.js
+++ src/App.js
@@ -10,10 +10,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+         onClick={()=> alert('here')}
         >
           Learn React
         </a>
